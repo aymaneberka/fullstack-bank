a8f53719bf0da315a5214e6c8e3299815d5bef8e
aymaneberka <128257148+aymaneberka@users.noreply.github.com>
2025-05-01 19:57:31 +0100

    Update index.ts

--- app/frontend/service/index.ts
+++ app/frontend/service/index.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
+const API_URL = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3001';
 
 const headers = {
   Accept: 'application/json, text/plain, */*',
