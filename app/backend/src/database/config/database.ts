3cc7b5c92ad74ddd66c4ebdbf7e5515408497018
aymaneberka <128257148+aymaneberka@users.noreply.github.com>
2025-06-02 00:06:50 +0100

    Update database.ts

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -2,8 +2,8 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.DB_USER ?? 'root',
-  password: process.env.DB_PASS ?? '123456',
+  username: process.env.DB_USER ?? 'admin',
+  password: process.env.DB_PASS ?? 'admin123',
   database: 'BANK',
   host: process.env.DB_HOST ?? 'localhost',
   port: Number(process.env.DB_PORT) || 3002,
